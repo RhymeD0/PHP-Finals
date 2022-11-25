301672531ab33108a59afce49874c795325e9732
BryanRoiDLR <94884047+BryanRoiDLR@users.noreply.github.com>
2022-11-25 13:08:30 +0800

    Update login.php

--- login.php
+++ login.php
@@ -43,12 +43,13 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         <title>Login</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
     <div class="center">
+    <strong><a>USER ACCOUNT</a></strong><br>
+    <a>Manage your account transactions and reservations</a><br>
     <form class="formCF" name="createAccountForm" action="login.php" method="post">
-        <strong><a class="titleHeading">USER ACCOUNT</a></strong><br>
-        <a>Manage your account transactions and reservations</a><br>
         <a>Administrative ID / Transaction ID</a><br>
         <input class="inputBox" type="text" name="id" id="id" required size="30"><br>
         <a>Password</a><br>
