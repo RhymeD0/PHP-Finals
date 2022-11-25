3a6652fbf9c44ddb542ddfeec115da3244656612
BryanRoiDLR <94884047+BryanRoiDLR@users.noreply.github.com>
2022-11-25 12:03:12 +0800

    Add files via upload

---
+++ reservation_admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>ROOMS AVAILABLE: </h1>
+        <h3>Single</h3>
+        <h3>Double</h3>
+        <h3>Triple</h3>
+        <h3>Quadruple</h3>
+        <h3>Queen</h3>
+        <h3>King</h3>
+
+        <h2>Confirmed Reservation: </h2>
+        <h2>Pending Reservation:</h2>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
